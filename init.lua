b72eaae64ed6923fd71bacda4d1c1ef328303e7d
Julian Torres <julian.torres@mongodb.com>
2022-12-27 18:39:44 -0500

    WIP: need to figure out why `<space>` isn't being accepted as the `mapleader`. Straight-up doesn't work.

--- init.lua
+++ init.lua
@@ -1,1 +1,8 @@
+-- LOL THIS STILL DOESN'T WORK I'M LOSING MY MIND
+vim.keymap.set("n", " ", "<nop>")
+
+-- Set leader keys as eary as possible.
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
 require("main")
