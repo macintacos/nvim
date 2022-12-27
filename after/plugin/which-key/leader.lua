a253fa386258d84499a559d09eaf09404e9fc7bd
Julian Torres <julian.torres@mongodb.com>
2022-12-27 18:31:05 -0500

    feat: setup whichkey config

--- after/plugin/which-key/leader.lua
+++ after/plugin/which-key/leader.lua
@@ -1,2 +1,6 @@
--- Main
+local wk = require("which-key")
 
+wk.register({
+    -- Buffer switching
+    ["1"] = { "<Cmd>BufferLineGoToBuffer 1<CR>", "which_key_ignore" },
+}, { prefix = "<leader>" })
