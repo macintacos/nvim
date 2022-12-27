005948c1a988661dfb8d959e9d9c300bd90eaa3a
Julian Torres <julian.torres@mongodb.com>
2022-12-27 18:36:15 -0500

    feat: add lazy's status output to lualine to show how many packages there are to update

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,5 +1,14 @@
 require("lualine").setup({
     options = {
         globalstatus = true
-    }
+    },
+    sections = {
+        lualine_x = {
+            {
+                require("lazy.status").updates,
+                cond = require("lazy.status").has_updates,
+                color = { fg = "#ff9e64" },
+            },
+        },
+    },
 })
