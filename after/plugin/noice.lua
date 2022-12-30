41f92c98f73b0689486b64141573615bd76570ea
Julian Torres <julian.torres@mongodb.com>
2022-12-30 14:25:55 -0500

    feat(noice): make some adjustments to how noice is set up

--- after/plugin/noice.lua
+++ after/plugin/noice.lua
@@ -6,11 +6,37 @@ require("noice").setup({
             ["vim.lsp.util.stylize_markdown"] = true,
             ["cmp.entry.get_documentation"] = true,
         },
+        signature = { enabled = false },
+        progress = { enabled = false },
+    },
+
+    routes = {
+        {
+            -- Use this route if you want to disable the search matches
+            filter = { event = "msg_show", kind = "search_count" },
+            opts = { skip = true },
+        },
+    },
+
+    views = {
+        mini = {
+            align = "message-left", -- FIXME: Can't seem to get this to work.
+        },
+        cmdline_popup = {
+            position = {
+                row = 5,
+                col = "50%",
+            },
+        },
+        popupmenu = {
+            relative = "editor",
+            position = "auto"
+        },
     },
 
     -- you can enable a preset for easier configuration
     presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
+        bottom_search = false, -- use a classic bottom cmdline for search
         command_palette = true, -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = true, -- enables an input dialog for inc-rename.nvim
