a25f1b2742f882deb73d2e43800c94498647d1a4
Julian Torres <julian.torres@mongodb.com>
2022-12-30 14:16:10 -0500

    feat: adjust helper functions for mapping so that they work properly, and add `vmap`

--- lua/main/helpers.lua
+++ lua/main/helpers.lua
@@ -2,8 +2,7 @@ local M = {}
 
 --[[ MAPPING HELPERS ]]
 
----Convenience function that binds a mode to a `vim.keymap.set`.
----@param mode string|table The mode to map.
+---@alias mode string|table The mode to map.
 ---| '""' # Regular noremap (all modes)
 ---| '"n"' # Normal mode
 ---| '"i"' # Insert mode
@@ -12,29 +11,33 @@ local M = {}
 ---| '"v"' # Visual _and_ Select modes
 ---| '"c"' # Command-line mode
 ---| '"o"' # Operator-pending mode
+
+---Convenience function that binds a mode to a `vim.keymap.set`.
+---@param op mode
 ---@param outer_opts table? Optional take of options to pass when binding.
-local function bind(mode, outer_opts)
+local function bind(op, outer_opts)
     -- Defaults
     outer_opts = outer_opts or { noremap = true, silent = true }
 
     ---Convenience function to create a keymap for mode that has been bound: "{mode}"
+    ---     FYI: DO NOT CHANGE THE FUNCTION SIGNATURE OF THIS! It will break in weird ways, since
+    ---     it expects things to be named "op"/"lhs"/"rhs"/"opts". Fun!
     ---@param lhs string The mapping to set.
     ---@param rhs string|function The command to execute when the mapping is used.
-    ---@param mode_override? string|table The mode to override, if necessary.
     ---@param opts? table Options to pass. These are the same options as for `vim.api.nvim_set_keymap`.
-    return function(lhs, rhs, mode_override, opts)
+    return function(lhs, rhs, opts)
         -- Defaults
         opts = vim.tbl_extend("force",
             outer_opts,
             opts or {}
         )
-        mode = mode_override or mode
 
-        vim.keymap.set(mode, lhs, rhs, opts)
+        vim.keymap.set(op, lhs, rhs, opts)
     end
 end
 
-M.nmap = bind("n", { noremap = false })
+M.nmap = bind("n", { noremap = false, silent = true })
+M.vmap = bind("v", { noremap = false, silent = true })
 M.noremap = bind("")
 M.nnoremap = bind("n")
 M.inoremap = bind("i")
@@ -42,6 +45,5 @@ M.xnoremap = bind("x")
 M.vnoremap = bind("v")
 M.cnoremap = bind("c")
 M.onoremap = bind("o")
-M.flexnoremap = bind({}) -- use this when you want to specify a table of modes manually
 
 return M
