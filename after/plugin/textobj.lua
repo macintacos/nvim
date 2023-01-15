5beab3b9ea077694412f86b61a61e7ee4efe2941
Julian Torres <julian.torres@mongodb.com>
2023-01-15 18:24:13 -0500

    refactor: remove textobj file (not really necessary)

--- after/plugin/textobj.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd([[
-    augroup textobj_sentence
-        autocmd!
-        autocmd FileType markdown call textobj#sentence#init()
-        autocmd FileType textile call textobj#sentence#init()
-    augroup END
-]])
