406db1889b62815f2a27a3fc43cfe848115a077a
Julian Torres <julian.torres@mongodb.com>
2023-01-15 22:32:12 -0500

    fix: ignore certain directories from searching

--- after/plugin/fzf.lua
+++ after/plugin/fzf.lua
@@ -1,5 +1,7 @@
 require("fzf-lua").setup({
     fzf_colors = {
         ["bg"] = { "bg", "CursorLine" }
-    }
+    },
+    file_ignore_patterns = { "neodev.nvim", "~/.local", "/opt/homebrew" }
 })
+
