2cfc36d7750acf54753ac7ccdecf5c29d89029cc
Julian Torres <julian.torres@mongodb.com>
2023-01-15 22:33:01 -0500

    feat: force lazygit config to be parsed as yaml

--- lua/main/autocmds.lua
+++ lua/main/autocmds.lua
@@ -1,16 +1,23 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
-local yank_group = augroup('HighlightYank', {})
-
-autocmd('TextYankPost', {
+-- Highlight text when yanking
+local yank_group = augroup("HighlightYank", {})
+autocmd("TextYankPost", {
     group = yank_group,
-    pattern = '*',
+    pattern = "*",
     callback = function()
         vim.highlight.on_yank({
-            higroup = 'IncSearch',
+            higroup = "IncSearch",
             timeout = 150,
         })
     end,
 })
 
+-- Set lazygit file to yaml
+autocmd({"BufRead", "BufEnter"}, {
+    pattern = "*lazygit*",
+    callback = function()
+        vim.opt_local.filetype = "yaml"
+    end,
+})
