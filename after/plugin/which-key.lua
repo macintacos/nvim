0bd56662da3542dbd08948ff42e5f3f36a6f0a11
Julian Torres <julian.torres@mongodb.com>
2023-01-15 20:11:22 -0500

    feat(which-key, legendary): setup legendary before which-key

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -1,1 +1,5 @@
+-- For legendary.nvim to get which-key suggestions, needs to be loaded before which-key
+require("legendary").setup({ which_key = { auto_register = true } })
+
+-- Require which-key
 require("main.which-key")
