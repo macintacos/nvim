6bf27c5a38d32872514625f99ac072e4233eaa46
Julian Torres <julian.torres@mongodb.com>
2023-03-13 00:58:53 -0400

    feat: add `modes.nvim`, and remove configurations that aren't necessary anymore

--- after/plugin/appearance.lua
+++ after/plugin/appearance.lua
@@ -14,11 +14,5 @@ vim.cmd([[
     hi! MiniCursorwordCurrent gui=nocombine guifg=NONE guibg=NONE
 ]])
 
--- Cursor-related changes
-hi(0, "Cursor", { bg = palette.pink.bright, fg = palette.pink.bright })
-hi(0, "InsertCursor", { fg = palette.fg1, bg = palette.cyan.bright })
-hi(0, "VisualCursor", { fg = palette.bg1, bg = palette.orange.bright })
-hi(0, "CursorLine", { bg = palette.bg1 }) -- don't show the cursor line
-
 -- Window Borders
 hi(0, "VertSplit", { fg = palette.sel0 })
--- lua/main/autocmds.lua
+++ lua/main/autocmds.lua
@@ -1,19 +1,6 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
--- Highlight text when yanking
-local yank_group = augroup("HighlightYank", {})
-autocmd("TextYankPost", {
-    group = yank_group,
-    pattern = "*",
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = "IncSearch",
-            timeout = 150,
-        })
-    end,
-})
-
 -- Set a bunch of config files to yaml
 autocmd({ "BufRead", "BufEnter" }, {
     pattern = { "*lazygit*", "*yamlfmt*", "*yamllint*" },
