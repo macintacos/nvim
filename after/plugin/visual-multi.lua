26c43a4f147b6b501eacc45bfcec0e1de409753e
Julian Torres <julian.torres@mongodb.com>
2024-03-02 11:07:51 -0500

    fix: remove undotree and visual-multi files (forgot to do this before)

--- after/plugin/visual-multi.lua
+++
@@ -1,8 +0,0 @@
--- Plug 'mg979/vim-visual-multi'
--- Description: multiple cursors!
-local nmap = require("main.helpers").nmap
-local nnoremap = require("main.helpers").nnoremap
-
--- custom remaps (full list: https://github.com/mg979/vim-visual-multi/wiki/Mappings)
-nnoremap("<C-k>", "<Plug>(VM-Add-Cursor-Up)")
-nnoremap("<C-j>", "<Plug>(VM-Add-Cursor-Down)")
