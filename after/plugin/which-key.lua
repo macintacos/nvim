71f260a10833e2175415fe0bf345a86d8e32bd1f
Julian Torres <julian.torres@mongodb.com>
2024-03-02 11:08:11 -0500

    fix(which-key): adjustments to handle console warnings

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -1,5 +1,5 @@
 -- For legendary.nvim to get which-key suggestions, needs to be loaded before which-key
-require("legendary").setup({ which_key = { auto_register = true } })
+require("legendary").setup({ extensions = { which_key = { auto_register = true } } })
 
 -- Require which-key
 require("main.which-key")
