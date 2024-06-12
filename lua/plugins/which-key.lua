d05edf39e9399b2e22c247cbc8229fe4d8b97f74
Julian Torres <julian.torres@mongodb.com>
2024-06-11 20:15:26 -0400

    plugins(which-key): adjust how options and keymaps are set

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -14,11 +14,78 @@ return {
     local Cmd = require("config.helpers").Cmd
     local del = vim.keymap.del
 
+    -- [[ OPTIONS ]]
+    -- Override defaults
+    opts = {
+      defaults = opts.defaults,
+      plugins = {
+        marks = true, -- shows a list of your marks on ' and `
+        registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+        -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+        -- No actual key bindings are created
+        spelling = {
+          enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+          suggestions = 20, -- how many suggestions should be shown in the list?
+        },
+        presets = {
+          operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+          motions = true, -- adds help for motions
+          text_objects = true, -- help for text objects triggered after entering an operator
+          windows = true, -- default bindings on <c-w>
+          nav = true, -- misc bindings to work with windows
+          z = true, -- bindings for folds, spelling and others prefixed with z
+          g = true, -- bindings for prefixed with g
+        },
+      },
+      -- add operators that will trigger motion and text object completion
+      -- to enable all native operators, set the preset / operators plugin above
+      operators = { gc = "Comments" },
+      key_labels = {
+        -- override the label used to display some keys. It doesn't effect WK in any other way.
+        -- For example:
+        ["<space>"] = "SPC",
+        ["<cr>"] = "RET",
+        ["<tab>"] = "TAB",
+      },
+      icons = {
+        breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+        separator = "➜", -- symbol used between a key and it's label
+        group = "+", -- symbol prepended to a group
+      },
+      window = {
+        border = "double", -- none, single, double, shadow
+        position = "bottom", -- bottom, top
+        margin = { 1, 3, 2, 3 }, -- extra window margin [top, right, bottom, left]
+        padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+      },
+      layout = {
+        height = { min = 4, max = 25 }, -- min and max height of the columns
+        width = { min = 20, max = 50 }, -- min and max width of the columns
+        spacing = 3, -- spacing between columns
+        align = "center", -- align columns left, center or right
+      },
+      ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
+      hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
+      show_help = false, -- show help message on the command line when the popup is visible
+      show_keys = false, -- don't show keys as they are typed (better interaction with noice.nvim)
+      triggers = "auto", -- automatically setup triggers
+      triggers_blacklist = {
+        -- list of mode / prefixes that should never be hooked by WhichKey
+        -- this is mostly relevant for key maps that start with a native binding
+        -- most people should not need to change this
+        i = { "j", "k" },
+        v = { "j", "k" },
+      },
+    }
     wk.setup(opts)
-    wk.register(opts.defaults)
+
+    -- [[ MAPPINGS ]]
+    -- wk.register(opts.defaults) -- TODO: maybe doesn't need to be here? Seems to get set anyway
 
     -- Delete top-level menu items here that you want to get overridden
     del("n", "<leader>h")
+    del("n", "<leader>e")
+    del("n", "<leader>E")
 
     wk.register({
       -- Buffers
@@ -116,16 +183,4 @@ return {
       },
     }, { prefix = "<leader>" })
   end,
-  -- opts = function(_, opts)
-  --   -- Handle adding
-  --   require("config.which-keymaps").set_keymaps(opts)
-  --
-  --   opts = {
-  --     defaults = {
-  --     ["<leader>h"] = nil
-  --     },
-  --   }
-  --
-  --   return opts
-  -- end,
 }
