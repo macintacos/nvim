5cc5b0758e5fcc23dda0f0381329a0af1c67f498
Julian Torres <julian.torres@mongodb.com>
2024-11-11 12:53:48 -0500

    feat(plugins): add `helpview` and `tiny-inline-diagnostic`

---
+++ lua/plugins/helpview.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "OXY2DEV/helpview.nvim",
+    lazy = true,
+    ft = "help",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
+}
---
+++ lua/plugins/tiny-inline-diagnostic.lua
@@ -0,0 +1,19 @@
+vim.diagnostic.config({ virtual_text = false })
+
+return {
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    config = function()
+      require("tiny-inline-diagnostic").setup({
+        options = {
+          show_source = true,
+          multiple_diag_under_cursor = true,
+          multilines = true,
+          show_all_diagns_on_cursorline = true,
+        },
+      })
+    end,
+  },
+}
