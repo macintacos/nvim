26fa9cfdc9b0896a9d8a5638fc6953ef4175e5ce
Julian Torres <julian.torres@mongodb.com>
2025-01-02 12:59:20 -0500

    feat!(lazy): remove `mini-indentscope` lazy config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,7 +32,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lsp.neoconf" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.ui.edgy" },
-    { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     { import = "lazyvim.plugins.extras.util.dot" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
