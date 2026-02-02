fc5aaf852c9ae590b1c7b089d570fc87f2f455c2
Julian Torres <julian.torres@mongodb.com>
2026-02-02 16:07:18 -0500

    style(lazy): condense spec import to single line

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,10 +23,7 @@ require("config.highlights")
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    -- import your plugins
-    { import = "plugins" },
-  },
+  spec = { { import = "plugins" } },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
