a26a5314b06a16f4c1fc8de74b71a4947b7ecab4
Julian Torres <julian.torres@mongodb.com>
2026-02-02 16:07:12 -0500

    refactor(tiny-inline-diagnostic): simplify configuration
    - Remove custom options (use plugin defaults)
    - Move virtual_text disable into config function
    - Remove unnecessary nested table structure

--- lua/plugins/tiny-inline-diagnostic.lua
+++ lua/plugins/tiny-inline-diagnostic.lua
@@ -1,24 +1,14 @@
 -- github.com/rachartier/tiny-inline-diagnostic.nvim
 -- Tiny inline diagnostics
 
-vim.diagnostic.config({ virtual_text = false })
-
 ---@module "lazy"
 ---@type LazySpec
 return {
-  {
-    "rachartier/tiny-inline-diagnostic.nvim",
-    event = "VeryLazy",
-    priority = 1000,
-    config = function()
-      require("tiny-inline-diagnostic").setup({
-        options = {
-          show_source = true,
-          multiple_diag_under_cursor = true,
-          multilines = true,
-          show_all_diagns_on_cursorline = true,
-        },
-      })
-    end,
-  },
+  "rachartier/tiny-inline-diagnostic.nvim",
+  event = "VeryLazy",
+  priority = 1000,
+  config = function()
+    require("tiny-inline-diagnostic").setup()
+    vim.diagnostic.config({ virtual_text = false })
+  end,
 }
