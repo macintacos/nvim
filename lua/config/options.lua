a8688089d62ba6db8fc5bb8d3c1ab5eb2765fcf6
Julian Torres <julian.torres@mongodb.com>
2026-02-02 16:06:56 -0500

    refactor(options): clean up duplicate and unused options
    - Remove duplicate settings (foldlevel, ignorecase, ruler)
    - Remove colorcolumn setting
    - Remove commented winbar code
    - Consolidate inccommand to 'split' (was 'nosplit')
    - Keep list=true (remove conflicting list=false)
    - Enhance shortmess with additional flags (a, s)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -50,7 +50,6 @@ opt.autowrite = true -- Enable auto write
 -- only set clipboard if not in ssh, to make sure the OSC 52
 -- integration works automatically.
 opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
-opt.colorcolumn = "88" -- Column color
 opt.completeopt = "menu,menuone,noselect"
 opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
 opt.confirm = true -- Confirm to save changes before exiting modified buffer
@@ -65,7 +64,6 @@ opt.fillchars = {
   eob = " ",
 }
 opt.foldlevel = 99
-opt.foldlevel = 99
 opt.foldmethod = "indent"
 opt.foldtext = ""
 opt.formatoptions = "jcroqlnt" -- tcqj
@@ -73,26 +71,20 @@ opt.grepformat = "%f:%l:%c:%m"
 opt.grepprg = "rg --vimgrep"
 opt.hidden = true -- Abandoned buffers are hidden instead
 opt.ignorecase = true -- Ignore case
-opt.ignorecase = true -- Ignore case
-opt.inccommand = "nosplit" -- preview incremental substitute
 opt.inccommand = "split" -- Preview replacement commands with a split window
 opt.jumpoptions = "view"
 opt.laststatus = 3 -- global statusline
 opt.linebreak = true -- Wrap lines at convenient points
-opt.list = false -- Remove characters like tabs, trailing spaces, etc.
 opt.list = true -- Show some invisible characters (tabs...
 opt.mouse = "a" -- Enable mouse mode
 opt.number = true -- Print line number
 opt.pumblend = 10 -- Popup blend
 opt.pumheight = 10 -- Maximum number of entries in a popup
 opt.relativenumber = true -- Relative line numbers
-opt.ruler = false -- Disable the default ruler
-opt.ruler = true -- Show cursor position line and column
 opt.scrolloff = 4 -- Lines of context
 opt.sessionoptions = { "blank", "buffers", "curdir", "tabpages", "winsize", "winpos", "help", "globals", "skiprtp", "folds", "terminal", "localoptions" }
 opt.shiftround = true -- Round indent
 opt.shiftwidth = 2 -- Size of an indent
-opt.shortmess:append({ W = true, I = true, c = true, C = true })
 opt.shortmess:append({ W = true, I = true, c = true, C = true, a = true, s = true }) -- Avoid a bunch of "hit Enter" prompts
 opt.showcmd = true -- Show the command in the last line of the string
 opt.showmode = false -- Dont show mode since we have a statusline
@@ -109,17 +101,10 @@ opt.swapfile = false -- Disable the swapfile
 opt.tabstop = 2 -- Number of spaces tabs count for
 opt.termguicolors = true -- True color support
 opt.timeoutlen = vim.g.vscode and 1000 or 300 -- Lower than default (1000) to quickly trigger which-key
-
 opt.undofile = true
 opt.undolevels = 10000
 opt.updatetime = 200 -- Save swap file and trigger CursorHold
 opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
--- opt.winbar = table.concat({
---   "%#WinBar#",
---   " %.40t",
---   "%m",
---   "%r",
--- })
 opt.wildmenu = true -- Command line completion stuff
 opt.wildmode = "longest:full,full" -- Command-line completion mode
 opt.winminwidth = 5 -- Minimum window width
