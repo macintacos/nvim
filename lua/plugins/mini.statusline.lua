e6590053d7f4dc547193a1914d245bd233e2104d
Julian Torres <julian.torres@mongodb.com>
2026-02-02 16:47:12 -0500

    style(statusline): customize background color for devinfo and fileinfo sections
    Set MiniStatuslineDevinfo and MiniStatuslineFileinfo highlight groups
    to use #1a1a2e background color for better visual distinction

--- lua/plugins/mini.statusline.lua
+++ lua/plugins/mini.statusline.lua
@@ -1,58 +1,18 @@
 -- github.com/nvim-mini/mini.statusline
 -- Status line
 
--- Show lazy.nvim updates in statusline for 5 seconds once detected
-local show_lazy_updates = true
-local lazy_updates_timer_started = false
+-- Custom highlights for statusline sections
+local function set_statusline_highlights()
+  vim.api.nvim_set_hl(0, "MiniStatuslineDevinfo", { bg = "#1a1a2e" })
+  vim.api.nvim_set_hl(0, "MiniStatuslineFileinfo", { bg = "#1a1a2e" })
+end
+vim.api.nvim_create_autocmd("ColorScheme", { callback = set_statusline_highlights })
+set_statusline_highlights()
 
 ---@module "lazy"
 ---@type LazySpec
 return {
   "nvim-mini/mini.statusline",
   version = "*",
-  opts = {
-    content = {
-      active = function()
-        local MiniStatusline = require("mini.statusline")
-
-        -- Standard sections
-        local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-        local git = MiniStatusline.section_git({ trunc_width = 40 })
-        local diff = MiniStatusline.section_diff({ trunc_width = 75 })
-        local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-        local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
-        local filename = MiniStatusline.section_filename({ trunc_width = 140 })
-        local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
-        local location = MiniStatusline.section_location({ trunc_width = 75 })
-        local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
-
-        -- Lazy updates section (only shown for 5 seconds once updates are detected)
-        local lazy_updates = ""
-        if show_lazy_updates then
-          local lazy_status = require("lazy.status")
-          if lazy_status.has_updates() then
-            lazy_updates = " " .. lazy_status.updates() .. " plugin updates found"
-            -- Start the 5-second timer only once, when updates are first detected
-            if not lazy_updates_timer_started then
-              lazy_updates_timer_started = true
-              vim.defer_fn(function()
-                show_lazy_updates = false
-                vim.cmd("redrawstatus")
-              end, 5000)
-            end
-          end
-        end
-
-        return MiniStatusline.combine_groups({
-          { hl = mode_hl, strings = { mode } },
-          { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
-          "%<", -- Mark general truncate point
-          { hl = "MiniStatuslineFilename", strings = { filename } },
-          "%=", -- End left alignment
-          { hl = "MiniStatuslineFileinfo", strings = { lazy_updates, fileinfo } },
-          { hl = mode_hl, strings = { search, location } },
-        })
-      end,
-    },
-  },
+  opts = {},
 }
