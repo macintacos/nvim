235948799fa6f0fee64a803a60ab6bb1337b6a83
Julian Torres <julian.torres@mongodb.com>
2026-02-20 19:22:03 -0500

    fix(flash): configure explicit keybindings
    bind s to flash jump and disable S in normal/visual/operator modes

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -6,6 +6,7 @@
 return {
   "folke/flash.nvim",
   keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "S", mode = { "n", "x", "o", "v" }, false },
   },
 }
