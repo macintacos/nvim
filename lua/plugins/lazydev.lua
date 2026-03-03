ce723baeabcaa023a8d1a15f14d0706e825a10e3
Julian Torres <julian.torres@mongodb.com>
2026-03-03 18:39:39 -0500

    fix(lazydev): correct lazy.nvim library word trigger

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -6,7 +6,7 @@ return {
     library = {
       { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       { path = "snacks.nvim", words = { "Snacks" } },
-      { path = "lazy.nvim", words = { "LazyVim" } },
+      { path = "lazy.nvim", words = { "LazySpec" } },
     },
   },
 }
