33588f82cd8abe10ef69c075967f620f39f0932f
Julian Torres <julian@excessive.dev>
2026-04-20 03:19:54 -0400

    feat(rust): add rustfmt formatting via conform.nvim

--- plugin/conform.lua
+++ plugin/conform.lua
@@ -12,6 +12,7 @@ vim.api.nvim_create_autocmd("BufWritePre", {
       formatters_by_ft = {
         lua = { "stylua" },
         python = { "ruff_format" },
+        rust = { "rustfmt" },
         sh = { "shfmt" },
         yaml = { "yamlfmt" },
       },
@@ -39,6 +40,7 @@ vim.api.nvim_create_user_command("ConformInfo", function()
     formatters_by_ft = {
       lua = { "stylua" },
       python = { "ruff_format" },
+      rust = { "rustfmt" },
       sh = { "shfmt" },
       yaml = { "yamlfmt" },
     },
