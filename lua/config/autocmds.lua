0a8f894ba0d8994332781a1f75ebbcf3ef2fab5e
Julian Torres <julian.torres@mongodb.com>
2026-05-13 21:52:16 -0500

    feat: add tmpl support

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -217,3 +217,36 @@ vim.api.nvim_create_autocmd({ "WinLeave" }, {
     vim.wo.cursorcolumn = false
   end,
 })
+
+-- Highlight .tmpl files properly, using ../../syntax/gotmpl.vim
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  group = vim.api.nvim_create_augroup("gotmpl_highlight", { clear = true }),
+  pattern = "*.tmpl",
+  callback = function()
+    local filename = vim.fn.expand("%:t")
+    local ext = filename:match(".*%.(.-)%.tmpl$")
+
+    -- Add more extension to syntax mappings here if you need to.
+    local ext_filetypes = {
+      go = "go",
+      html = "html",
+      json = "json",
+      md = "markdown",
+      yaml = "yaml",
+      yml = "yaml",
+      fish = "fish",
+    }
+
+    if ext and ext_filetypes[ext] then
+      -- Set the primary filetype
+      vim.bo.filetype = ext_filetypes[ext]
+
+      -- Define embedded Go template syntax
+      vim.cmd([[
+        syntax include @gotmpl syntax/gotmpl.vim
+        syntax region gotmpl start="{{" end="}}" contains=@gotmpl containedin=ALL
+        syntax region gotmpl start="{%" end="%}" contains=@gotmpl containedin=ALL
+      ]])
+    end
+  end,
+})
